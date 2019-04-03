469726eec38019e6857dcccabdb9a8e3949665c9
MaeganW <maeganwomble@gmail.com>
2019-04-03 11:02:02 -0500

    added full resource methods to contacts controller

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -7,16 +7,35 @@ use App\Contact;
 
 class ContactsController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         $contacts = Contact::all();
         return view('contacts', ['contacts' =>  $contacts]);
     }
 
-    public function create(){
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
         return view('form');
     }
 
-    public function store(){
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
         $contact = new Contact();
         $contact->firstName = request('firstName');
         $contact->lastName = request('lastName');
@@ -25,17 +44,50 @@ class ContactsController extends Controller
         $contact->phone = request('phone');
         $contact->save();
         return redirect('/contacts');
-        // return request()->all();
     }
 
-
-
-
-    public function profile(){
-        return view('profile');
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
     }
 
-    public function home(){
-        return view('home');
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
